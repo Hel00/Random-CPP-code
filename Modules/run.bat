b2b05401aed0fc18072ecdeadc0d96d1b44a2384
Hel <11891920+Hel00@users.noreply.github.com>
2022-07-11 11:15:13 +0200

    Update run.bat

--- Modules/run.bat
+++ Modules/run.bat
@@ -2,3 +2,6 @@ set CXX=clang++ --std=c++20 -fmodules-ts -Oz -s --target=x86_64-pc-windows-gnu
 
 %CXX% -o split.pcm  --precompile -x c++-module split.cpp
 %CXX% -o main.exe -fprebuilt-module-path=. main.cpp split.cpp
+
+z++ --std=c++20 -fmodules-ts --precompile -x c++-module -c split.cpp -o split.pcm
+z++ --std=c++20 -fmodules-ts -o main.exe -fprebuilt-module-path=. main.cpp split.cpp

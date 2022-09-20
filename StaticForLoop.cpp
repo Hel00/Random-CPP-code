3f2f3bede61225d61b01843f073ad78ed30b5d10
Hel <11891920+Hel00@users.noreply.github.com>
2022-09-20 11:53:27 +0200

    Update StaticForLoop.cpp

--- StaticForLoop.cpp
+++ StaticForLoop.cpp
@@ -29,3 +29,36 @@ int main()
         );
     });
 }
+
+/* OR THIS */
+
+#include <concepts>
+
+template <int First, int Last>
+struct static_for
+{
+    template <typename Lambda>
+    static inline constexpr void apply(Lambda const& f)
+    {
+        if constexpr (First < Last)
+        {
+            f(std::integral_constant<int, First>{});
+            static_for<First + 1, Last>::apply(f);
+        }
+    }
+};
+
+template <int N>
+struct static_for<N, N>
+{
+    template <typename Lambda>
+    static inline constexpr void apply(Lambda const& f) {}
+};
+
+int main()
+{
+    static_for<0, 10>::apply([&](auto i)
+    {            
+        asm("nop");
+    });
+}

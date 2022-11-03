a4b10260c57016e0e50b1dd7cb098eb9a8d7a56d
Hel <11891920+Hel00@users.noreply.github.com>
2022-11-03 01:14:00 +0100

    Update optionImplementation.cpp

--- optionImplementation.cpp
+++ optionImplementation.cpp
@@ -31,3 +31,32 @@ int main()
   Option<void*> option;
   Option optionSome(42);
 }
+
+// Kinda implemented
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+template< typename T >
+struct Option
+{
+  T value;
+};
+
+template<>
+struct Option<void*>
+{
+};
+
+int main()
+{
+  Option<int> o1{2};
+  Option<float> o2{2.42};
+  Option<void*> o3{};
+
+  cout << "Option `int` is " << o1.value << endl;
+  cout << "Option `float` is " << o2.value << endl;
+  //cout << "option `void*` is " << o3.value << endl;
+}

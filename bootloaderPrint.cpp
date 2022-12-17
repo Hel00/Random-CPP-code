cb710adb315da24403cffd956ab2c6b80fb2d679
Hel <11891920+Hel00@users.noreply.github.com>
2022-12-17 09:15:06 +0100

    Create bootloaderPrint.cpp

---
+++ bootloaderPrint.cpp
@@ -0,0 +1,46 @@
+/* COMPILED WITH */
+
+// z++ main.cpp --target=i386-freestanding-none -Oz -s -c -m16
+// objcopy -O binary main.o main.bin --change-section-address .text=0x7C00
+// qemu-system-x86_64 -hda main.bin
+
+asm
+(
+  R"(
+    .intel_syntax;
+
+    _start:
+    mov ah, 0xe;
+    mov al, 'A';
+    //mov bx, 0;
+
+    .byte 0xBB
+    .byte 0x00
+    .byte 0x00
+
+    int 0x10;
+
+    loop:
+    jmp loop
+
+    .rept 249
+    .short 0x0000
+    .endr
+    .byte 0x00
+    .byte 0x55
+    .byte 0xAA
+
+    .att_syntax;
+  )"
+);
+
+/*
+00000000  B40E              mov ah,0xe
+00000002  B041              mov al,0x41
+00000004  BB0000            mov bx,0x0
+00000007  CD10              int 0x10
+00000009  EBFE              jmp short 0x9
+...
+0000
+0055AA
+*/

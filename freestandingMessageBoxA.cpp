a097483d485d56d11e5eec69bff89acecc080295
Hel <11891920+Hel00@users.noreply.github.com>
2023-02-02 00:48:03 +0100

    Update freestandingMessageBoxA.cpp

--- freestandingMessageBoxA.cpp
+++ freestandingMessageBoxA.cpp
@@ -1,6 +1,6 @@
-// zcc test.cpp -s --target=x86_64-windows -L"/home/hel/.wine/drive_c/windows/system32/" -l:kernel32.dll -l:user32.dll
+// zig build-exe -O ReleaseSmall -ffunction-sections --gc-sections --entry Main freestandingMessageBoxA.cpp
 
-// zig build-exe -target x86_64-windows -L"/home/hel/.wine/drive_c/windows/system32/" -dynamic -lkernel32 -luser32 -lc test.cpp
+// z++ freestandingMessageBoxA.cpp -Os -s --target=x86_64-windows-none -nostdlib -nostdinc -nostdinc++ -nostartfiles -L"/home/hel/.wine/drive_c/windows/system32/" "-Wl,--subsystem,native" -dynamic -lkernel32 -luser32
 
 extern "C" int MessageBoxA(
   int   hWnd,
@@ -13,10 +13,7 @@ extern "C" void Main() __attribute__((constructor));
 
 extern "C" void Main()
 {
-  //MessageBoxA(0, "Hello World!", "Output:", 0);
-}
-
-int main()
-{
   MessageBoxA(0, "Hello World!", "Output:", 0);
 }
+
+void wWinMainCRTStartup() { Main(); }

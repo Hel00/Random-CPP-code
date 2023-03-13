b2be8b4556fcffc760087a499b3d49e809d45ddb
Hel <11891920+Hel00@users.noreply.github.com>
2023-03-13 06:15:19 +0100

    Delete Modules directory

--- Modules/buildClang.sh
+++
@@ -1,6 +0,0 @@
-clang++ -std=c++20 -fmodules -fbuiltin-module-map -fmodules-ts -Xclang -emit-module-interface -c moduleTest.cpp -o moduleTest.pcm
-clang++ -std=c++20 -fmodules -fbuiltin-module-map -c moduleTest.cpp
-clang++ -std=c++20 -fmodules -fbuiltin-module-map -fprebuilt-module-path=. -c main.cpp -o main.o
-clang++ -std=c++20 -fmodules -fbuiltin-module-map main.o moduleTest.o -o main
-
-#-stdlib=libc++
--- Modules/moduleTest.cpp
+++
@@ -1,5 +0,0 @@
-module;
-
-export module moduleTest;
-
-export auto retInt() -> int { return 42; }

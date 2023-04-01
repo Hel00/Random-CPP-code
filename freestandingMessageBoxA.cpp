c3ee10721d6904924f15646cf1cdf0eeaf99f650
Hel <11891920+Hel00@users.noreply.github.com>
2023-04-01 17:09:18 +0200

    Update freestandingMessageBoxA.cpp

--- freestandingMessageBoxA.cpp
+++ freestandingMessageBoxA.cpp
@@ -1,6 +1,6 @@
 // zig build-exe -O ReleaseSmall -ffunction-sections --gc-sections --entry Main freestandingMessageBoxA.cpp
 
-// zig build-exe -O ReleaseSmall -ffunction-sections --gc-sections --entry Main -L"/home/hel/.wine/drive_c/windows/system32/" -dynamic -lkernel32 -luser32 freestandingMessageBoxA.cpp
+// zig build-exe -target x86_64-windows-none -L"/home/hel/.wine/drive_c/windows/system32/" -dynamic -lkernel32 -luser32 freestandingMessageBoxA.cpp
 
 // z++ freestandingMessageBoxA.cpp -Os -s --target=x86_64-windows-none -nostdlib -nostdinc -nostdinc++ -nostartfiles -L"/home/hel/.wine/drive_c/windows/system32/" "-Wl,--subsystem,native" -dynamic -lkernel32 -luser32
 

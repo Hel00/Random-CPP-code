21410bf4d516141839498c9c5e1aeae1d633e039
Hel <11891920+Hel00@users.noreply.github.com>
2023-04-04 19:43:46 +0200

    Add files via upload

---
+++ Modules/main.cpp
@@ -0,0 +1,10 @@
+import moduleTest;
+
+#include <cstdio>
+
+auto main() -> int
+{
+  printf("retInt is: %d", retInt());
+
+  return 0;
+}
---
+++ Modules/moduleTest.cpp
@@ -0,0 +1,6 @@
+module;
+
+export module moduleTest;
+
+export auto retInt() -> int { return 42; }
+auto retInt2() -> int { return 422; }

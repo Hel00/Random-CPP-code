5f284c82b32b39e36f24ae45a5b9b1721cab79e9
Hel <11891920+Hel00@users.noreply.github.com>
2023-04-08 05:34:48 +0200

    Update FixedString.cpp

--- FixedString.cpp
+++ FixedString.cpp
@@ -43,23 +43,26 @@ public:
 
     return inverted_string;
   }
-  consteval auto replaceAndFill() const
-  {
-    FixedString<stream.size + 10> temp{};
-
-    for (int index = 0; index <= stream.size; index++)
-    {
-      temp[index] = stream[index];
-    }
-
-    for (int index = stream.size; index < stream.size + 10; index++)
-    {
-      temp[index] = 'A';
-    }
-
-    return temp;
-  }
+  // Could make `replaceAndFill` a member function too just dont forget to mark it as `const`
 };
+
+template<FixedString stream>
+consteval auto replaceAndFill()
+{
+  FixedString<stream.size + 10> temp{};
+
+  for (int index = 0; index <= stream.size; index++)
+  {
+    temp[index] = stream[index];
+  }
+
+  for (int index = stream.size; index < stream.size + 10; index++)
+  {
+    temp[index] = 'A';
+  }
+
+  return temp;
+}
 
 #include <iostream>
 
@@ -74,7 +77,7 @@ int main()
 
   cout << "Inverted `FixedString` is: " << invertedStream << endl;
 
-  static constexpr auto replaced = fixedStringManager.replaceAndFill();
+  static constexpr auto replaced = replaceAndFill<invertedStream>();
 
   cout << "Replaced and filled `FixedString` is: " << replaced << endl;
 }

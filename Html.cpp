fd99e515d767511aa559ddf3b6ef71862d2250a0
Hel <11891920+Hel00@users.noreply.github.com>
2023-06-08 11:27:47 +0200

    Update Html.cpp

--- Html.cpp
+++ Html.cpp
@@ -89,3 +89,70 @@ int main()
 {
   Html h{ Element{}, Element{}, P{"Paragraph"}, Element{} };
 }
+
+
+
+// OR
+
+
+
+// https://stackoverflow.com/questions/4041447/how-is-stdtuple-implemented/52208842#52208842
+
+#include <iostream>
+#include <string>
+#include <tuple>
+
+template <typename... Tags>
+struct Html {
+    constexpr Html(Tags... args) : tags(args...) {}
+
+    template <typename Tag>
+    constexpr const Tag& get() const {
+        return std::get<Tag>(tags);
+    }
+
+    std::tuple<Tags...> tags;
+};
+
+struct h1 {
+    static constexpr std::string_view tag = "h1";
+    std::string_view content = "Header 1";
+};
+struct h2 {
+    static constexpr std::string_view tag = "h2";
+    std::string_view content = "Header 2";
+};
+struct b {
+    static constexpr std::string_view tag = "b";
+    std::string_view content = "Bold";
+};
+struct p {
+    static constexpr std::string_view tag = "p";
+    std::string_view content = "Paragraph";
+};
+
+template <typename... Tags>
+constexpr auto make_html(Tags&&... tags) {
+    return Html<std::decay_t<Tags>...>{std::forward<Tags>(tags)...};
+}
+
+template <typename... Tags>
+auto make_html2(Tags... tags)
+{
+  return Html{tags...}; 
+}
+
+constexpr auto html = make_html(h1{}, h2{}, b{}, p{});
+
+int main() {
+    // visitor function that prints the type and content of each tag
+    static constexpr auto visitor = [](const auto& tag) {
+        std::cout << tag.tag << ": " << tag.content << std::endl;
+    };
+
+    // get the h1 tag and print its content
+    static constexpr auto& h1_tag = html.get<h1>();
+    std::cout << h1_tag.content << std::endl;
+
+    return 0;
+}

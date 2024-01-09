d07216cfdbcb0619d36c9d3d54f18ee5f0037ce9
Hel <11891920+Hel00@users.noreply.github.com>
2024-01-09 03:58:36 +0100

    Update Box.cpp

--- Box.cpp
+++ Box.cpp
@@ -1,4 +1,47 @@
-#include <utility>
+template <typename T>
+struct removeReference
+{
+    using type = T;
+};
+
+template <typename T>
+struct removeReference<T&>
+{
+    using type = T;
+};
+
+template <typename T>
+struct removeReference<T&&>
+{
+    using type = T;
+};
+
+template <typename T>
+constexpr typename removeReference<T>::type &&move(T&& arg) noexcept
+{
+    return static_cast<typename removeReference<T>::type&&>(arg);
+}
+
+template <typename T>
+constexpr T&& forward(typename removeReference<T>::type& arg) noexcept
+{
+    return static_cast<T&&>(arg);
+}
+
+template <typename T>
+constexpr T&& forward(typename removeReference<T>::type&& arg) noexcept
+{
+
+    return static_cast<T&&>(arg);
+}
+
+template <typename T, typename U = T>
+constexpr T exchange(T& obj, U&& new_value)
+{
+    T old_value = move(obj);
+    obj = forward<U>(new_value);
+    return old_value;
+}
 
 template<typename DataType>
 struct Box
@@ -16,13 +59,13 @@ struct Box
   }
 
   // Move operations
-  constexpr Box(Box &&other) : data(std::exchange(other.data, nullptr)) {}
+  constexpr Box(Box &&other) : data(exchange(other.data, nullptr)) {}
   constexpr Box &operator = (Box &&other)
   {
     delete data;
     data = nullptr;
 
-    data = std::exchange(other.data, nullptr);
+    data = exchange(other.data, nullptr);
   }
 
   // Copy operators (they are deleted)
@@ -30,7 +73,6 @@ struct Box
   constexpr Box &operator = (const Box&) = delete;
 };
 
-// Testing `Box` in constexpr scope
 constexpr void boxTest()
 {
   Box<int> data(new int());

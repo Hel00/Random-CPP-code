2b543add7e046bb26ee7e890e7af964dc25d04ec
Hel <11891920+Hel00@users.noreply.github.com>
2024-01-29 22:34:08 +0100

    Update Option.cpp

--- Option.cpp
+++ Option.cpp
@@ -51,3 +51,60 @@ int main()
 
   process(optionSome, optionNone);
 }
+
+
+
+// OR
+
+
+
+#include <iostream>
+
+template<typename T>
+struct Option
+{
+  T data;
+
+  constexpr Option(T data) : data(data) {}
+
+  constexpr bool hasValue() { return true; }
+  constexpr T getData() { return this->data; }
+};
+
+template<>
+struct Option<void>
+{
+  struct None{};
+
+  constexpr Option() = default;
+
+  constexpr bool hasValue() { return false; }
+  constexpr None getData() { return None{}; }
+};
+
+template<typename T>
+Option(T data) -> Option<T>;
+Option() -> Option<void>;
+
+
+template<typename ...Type>
+constexpr void process(Option<Type> ...options)
+{
+  ([&]
+   {
+     if constexpr ( options.hasValue() == true )
+     {
+       std::cout << options.getData() << std::endl;
+     }
+     else {}
+
+   } (), ...);
+}
+
+int main()
+{
+  static constexpr Option some(42);
+  static constexpr Option none;
+
+  process(some, none);
+}

31c7d498cc6b5767720065793799cab7f5d8ee2c
Hel <11891920+Hel00@users.noreply.github.com>
2024-08-17 16:19:05 +0200

    Update main.cpp

--- Bf4_syscalls/main.cpp
+++ Bf4_syscalls/main.cpp
@@ -1,9 +1,10 @@
-// zig build-exe -target x86_64-windows-none -L"/home/hel/.wine/drive_c/windows/system32/" -dynamic -lkernel32 -luser32 -cflags --std=c++20 -- --subsystem windows --entry Main -O ReleaseSmall -fstrip -flto --gc-sections -ffunction-sections main.cpp
+// zig build-exe -target x86_64-windows-none -L"/home/hel/.wine/drive_c/windows/system32/" -dynamic -lkernel32 -luser32 -cflags --std=c++20 -- --subsystem windows -fentry=Main -O ReleaseSmall -fstrip -flto --gc-sections -ffunction-sections main.cpp
 
 #include "types.hpp"
 #include "win32Utils.hpp"
 #include "memory.hpp"
 
+// 0.5 = 0x3F000000
 // 0.75 = 0x3F400000
 // 0.85 = 0x3F59999A
 
@@ -12,7 +13,7 @@ extern "C" void Main()
   static constexpr const char *processName = "Battlefield 4";
   static constexpr auto PROCESS_ALL_ACCESS = (0x000F0000L | 0x00100000L | 0xFFF); // 2035711
   static constexpr auto delay = 10'000;
-  static constexpr auto recoil = 0x3F59999A;
+  static constexpr auto recoil = 0x3F000000;
 
   DWORD processId;
   HWND processHwnd = FindWindowA(0, processName);

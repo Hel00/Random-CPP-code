a256417356ebc3230ce12b2c507b6ea42d2a69d3
Hel <11891920+Hel00@users.noreply.github.com>
2025-02-27 07:08:00 +0100

    Update build.sh

--- Modules/build.sh
+++ Modules/build.sh
@@ -1,4 +1,4 @@
-clang++ -Oz -s -std=c++20 -fmodules -fbuiltin-module-map -fmodules-ts -Xclang -emit-module-interface -S -emit-llvm moduleTest.cpp -o moduleTest.pcm
+clang++ -Oz -s -std=c++20 -fmodules -fbuiltin-module-map -Xclang -emit-module-interface -S -emit-llvm moduleTest.cpp -o moduleTest.pcm
 clang++ -Oz -s -std=c++20 -fmodules -fbuiltin-module-map -S -emit-llvm moduleTest.cpp
 clang++ -Oz -s -std=c++20 -fmodules -fbuiltin-module-map -fprebuilt-module-path=. -S -emit-llvm main.cpp -o main.ll
 
